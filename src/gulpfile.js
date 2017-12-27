3eb6564b37f3e6cec0f3cc1f46db95199294d12c
marvin <mengfeng@chipwing.com>
2017-12-27 11:15:16 +0800

    feat(gulpfile): add gulpfile.js

---
+++ src/gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by marvin on 17/12/27.
+ */
+
+const gulp = require('gulp');
+
+// 转译javaScript
+gulp.task('webpack', () => {});
+
+// 编译 less --> css
+gulp.task('less', () => {});
+
+gulp.task('default', ['webpack', 'less']);
