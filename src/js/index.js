8da87377d7a348fd538f24d0b3ebb8464558332f
marvin <mengfeng@chipwing.com>
2017-12-27 14:51:43 +0800

    feat(index， toolkit): 抽取生成算法

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,10 @@
 /**
  * Created by marvin on 17/12/27.
  */
-
-function makeRow (v = 0) {
-  const array = new Array(9);
-  array.fill(v);
-  return array;
-}
-
-function makeMatrix (v = 0) {
-  // const array = new Array(9);
-  // array.fill(makeRow(v));
-  // return array;
-  return Array.from({ length: 9 }, () => makeRow(v));
-}
-
-/**
- * Fisher-Yates 洗牌算法
- * @param array
- */
-function shuffle (array) {
-  const endIndex = array.length - 2;
-
-  for (let i=0; i<=endIndex; i++) {
-    const j = i + Math.floor(Math.random() * (array.length - i));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-
-  return array;
-}
+// import toolkit from './toolkit';
+const toolkit = require('./toolkit');
 
 const a = Array.from({ length: 9 }, (v, i) => i);
 console.log(11, a);
 
-console.log(22, shuffle(a));
+console.log(22, toolkit.shuffle(a));
