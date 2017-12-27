3738dae3daf2505c7983c71ad732747f7b733939
marvin <mengfeng@chipwing.com>
2017-12-27 14:29:55 +0800

    feat(index): 二维数组

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,20 @@
 /**
  * Created by marvin on 17/12/27.
  */
+
+function makeRow (v = 0) {
+  const array = new Array(9);
+  array.fill(v);
+  return array;
+}
+
+function makeMatrix (v = 0) {
+  // const array = new Array(9);
+  // array.fill(makeRow(v));
+  // return array;
+  return Array.from({ length: 9 }, () => makeRow(v));
+}
+
+const a = makeMatrix();
+a[0][1] = 2;
+console.log(11, a);
