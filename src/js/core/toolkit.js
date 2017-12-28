d420a4d5aadf361ef57ade6cea620860520bac31
marvin <mengfeng@chipwing.com>
2017-12-28 11:43:27 +0800

    feat(index, generator, toolkit): 生成数独解决方案

--- src/js/core/toolkit.js
+++ src/js/core/toolkit.js
@@ -35,6 +35,23 @@ const matrixToolkit = {
     }
 
     return array;
+  },
+
+  /**
+   * 检查指定位置是否可以填写数字n
+   * @returns {boolean}
+   */
+  checkFillable (matrix, n, rowIndex, colIndex) {
+    const row = matrix[rowIndex];
+    const col = this.makeRow().map((v, i) => matrix[i][colIndex]);
+    const { boxIndex } = boxToolkit.convertToBoxIndex(rowIndex, colIndex);
+    const box = boxToolkit.getBoxCells(matrix, boxIndex);
+
+    for (let i = 0; i < 9; i++) {
+      if (row[i] === n || col[i] === n || box[i] === n) return false;
+    }
+
+    return true;
   }
 };
 
@@ -43,7 +60,33 @@ const matrixToolkit = {
  * @type {{}}
  */
 const boxToolkit = {
-  // todo
+  getBoxCells (matrix, boxIndex) {
+    const startRowIndex = Math.floor(boxIndex / 3) * 3;
+    const startColIndex = boxIndex % 3 * 3;
+    const result = [];
+
+    for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+      const rowIndex = startRowIndex + Math.floor(cellIndex / 3);
+      const colIndex = startColIndex + cellIndex % 3;
+      result.push(matrix[rowIndex][colIndex]);
+    }
+
+    return result;
+  },
+
+  convertToBoxIndex (rowIndex, colIndex) {
+    return {
+      boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+      cellIndex: rowIndex % 3 * 3 + colIndex % 3
+    };
+  },
+
+  convertFromBoxIndex (boxIndex, cellIndex) {
+    return {
+      rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(cellIndex / 3),
+      colIndex: boxIndex % 3 * 3 + cellIndex % 3
+    };
+  }
 };
 
 // 工具集
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
  * Created by marvin on 17/12/27.
  */
 const Grid = require('./ui/grid');
+const generator = require('./core/generator');
 
 const grid = new Grid($('#container'));
 grid.build();
